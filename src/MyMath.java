b97ee25b69da2102783170758ed629a220f20336
lance <fragasteven148@gmail.com>
2023-10-19 10:36:35 -0700

    Assignment-007-Steven Fraga

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
-
 public class MyMath {
 
+    public static int gCF(int a, int b) {
+        while (b!= 0) {
+            if (a>b) {
+                int c = b;
+                b = a % b;
+                a = c;
+            } else {
+                b = b % a;
+            }
+        } return a;
+    }
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Give a positive number: ");
+        int a = Integer.parseInt(s.nextLine());
+        System.out.println("Give a positive number: ");
+        int b = Integer.parseInt(s.nextLine());
+        System.out.println(gCF(a,b));
     }
 }
