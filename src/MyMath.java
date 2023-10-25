608e4c53dc7b4cbe3b283ee4798956eb2f429de3
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-25 00:01:29 -0700

    Part 1 >>>NONOPERATIONAL
    >>>MODIFICATIONS
    --STUB euclidGCF method
    
    >>>DEFICITS
    --
    
    >>>BUGS
    --
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Assignment-007\src\MyMath.java
      -Warning:(1, 1) Unused import statement
      -Warning:(5, 24) Method 'euclidGCF(int, int)' is never used
      -Warning:(5, 38) Parameter 'a' is never used
      -Warning:(5, 45) Parameter 'b' is never used
      -Warning:(6, 9) 'return' is unnecessary as the last statement in a 'void' method

--- src/MyMath.java
+++ src/MyMath.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class MyMath {
 
+    public static void euclidGCF(int a, int b) {
+        return;
+    }
+
     public static void main(String[] args) {
+
     }
 }
