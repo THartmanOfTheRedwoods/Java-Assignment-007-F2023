b76f08cf3e79b955deecf09186f25d630eb701d8
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-25 01:25:41 -0700

    Part 1 >>>OPERATIONAL
    >>>MODIFICATIONS
    --MyMath.java
      -CREATED euclidMod method
      -BUILT euclidGCF method
      -CREATED getUserVals method
      -CREATED showGCF method
    
    >>>DEFICITS
    --method to determine which userVal is larger
    --MyMath.java
      -euclidGCF method needs an if tree before the while loop for special cases (both userVals are equivilent, one userVal is a multiple of the other)
    
    >>>BUGS
    --if one userVal is a multiple of the other, output is erroneously zero, unless smaller number is entered first.
    
    >>>THROWS
    --

--- src/MyMath.java
+++ src/MyMath.java
@@ -2,11 +2,35 @@ import java.util.Scanner;
 
 public class MyMath {
 
-    public static void euclidGCF(int a, int b) {
-        return;
+    private static int gcf;
+    private static int userVal1;
+    private static int userVal2;
+    static Scanner s = new Scanner(System.in);
+
+    private static void getUserVals() {
+        userVal1 = Integer.parseInt(s.nextLine());
+        userVal2 = Integer.parseInt(s.nextLine());
+    }
+
+    private static int euclidMod(int dividend, int divisor) {
+        return dividend % divisor;
+    }
+
+    private static void euclidGCF() {
+        getUserVals();
+        while (userVal1 % userVal2 != 0) {
+            gcf = euclidMod(userVal1, userVal2);
+            userVal1 = userVal2;
+            userVal2 = gcf;
+        }
+        showGCF();
+    }
+
+    private static void showGCF(){
+        System.out.println(gcf);
     }
 
     public static void main(String[] args) {
-
+        euclidGCF();
     }
 }
