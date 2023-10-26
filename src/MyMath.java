1944b1dfed6f194b0ef2533512cf4e70cd507109
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-25 19:09:54 -0700

    Part 1 >>>OPERATIONAL
    >>>MODIFICATIONS
    --MyMath.java
      -MODIFIED euclidGCF method: if statement for special case userVal1 == userVal2
      -CREATED sortUserVals method
      -MODIFIED getUserVals method
    
    >>>DEFICITS
    --
    
    >>>BUGS
    --
    
    >>>THROWS
    --

--- src/MyMath.java
+++ src/MyMath.java
@@ -7,19 +7,31 @@ public class MyMath {
     private static int userVal2;
     static Scanner s = new Scanner(System.in);
 
+
     private static void getUserVals() {
         userVal1 = Integer.parseInt(s.nextLine());
         userVal2 = Integer.parseInt(s.nextLine());
+        sortUserVals(userVal1, userVal2);
     }
 
-    private static int euclidMod(int dividend, int divisor) {
+    private static void sortUserVals(int a, int b) {
+        if (a > b) {
+            userVal1 = b;
+            userVal2 = a;
+        }
+    }
+
+    private static int euclidModulo(int dividend, int divisor) {
         return dividend % divisor;
     }
 
     private static void euclidGCF() {
         getUserVals();
+        if (userVal1 == userVal2) {
+            gcf = userVal1;
+        }
         while (userVal1 % userVal2 != 0) {
-            gcf = euclidMod(userVal1, userVal2);
+            gcf = euclidModulo(userVal1, userVal2);
             userVal1 = userVal2;
             userVal2 = gcf;
         }
