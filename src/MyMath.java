a130afac66d4e95519b95af5bae125fd30ed95a1
Cassandra Portlock <stellarluna30@gmail.com>
2023-11-05 19:59:03 -0800

    completed assignment007

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,33 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Cassandra Portlock
+ *
+ * @since Version 1
+ *
+ */
+
 import java.util.Scanner;
 
 public class MyMath {
+    private final static Scanner s = new Scanner(System.in);
+    public static void gcf() {
+        System.out.print("Type first number to find greatest common factor: ");
+        int a = s.nextInt();
+
+        System.out.print("Type first number to find greatest common factor: ");
+        int b = s.nextInt();
+
+        while (b != 0) {
+            int gcf = a;
+            a = b;
+            b = gcf % b;
+        }
+
+        System.out.printf("The Greatest Common Factor is: %s", a);
+    }
 
     public static void main(String[] args) {
+        gcf();
     }
 }
