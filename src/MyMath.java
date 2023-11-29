1645c8c5a89d2ed0de28af78f7f2346a829009a6
MJFracess <mjfracess@gmail.com>
2023-11-29 06:38:21 -0800

    finally completed Assignment 007

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
 
+/**
+ * @author Trevor Hartman
+ * @author MJ Fracess
+ * @since Version 1.0
+ */
 public class MyMath {
+    private final static Scanner s = new Scanner(System.in);
+    public static void gcf (){
+        System.out.println("Enter your first number to find the greatest common factor:");
+        int a = s.nextInt();
+
+        System.out.println("Enter your second number to find the greatest common factor:");
+        int b = s.nextInt();
+
+        while (b!= 0){
+            int c = a;
+            a=b;
+            b = c % b;
+        }
+        System.out.printf("The Greatest Common Factor is: %d", a);
+    }
 
     public static void main(String[] args) {
+    gcf();
     }
 }
